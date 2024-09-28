44582ec23c5baa01373bcea37b4c91988c8fb0e6
Seungwan Yoo <ysw99@kakao.com>
2024-09-28 18:17:41 +0900

    first commit

---
+++ src/main/java/com/team1/DailyBox/DailyBoxApplication.java
@@ -0,0 +1,13 @@
+package com.team1.DailyBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyBoxApplication.class, args);
+	}
+
+}
