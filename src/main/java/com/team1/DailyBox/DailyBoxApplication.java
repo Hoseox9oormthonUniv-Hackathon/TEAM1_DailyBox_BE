176d7a8c520bd47cbb4d46cef7de28da8cfdd1db
Seungwan Yoo <ysw99@kakao.com>
2024-09-29 04:11:56 +0900

    서버 시간대 설정

--- src/main/java/com/team1/DailyBox/DailyBoxApplication.java
+++ src/main/java/com/team1/DailyBox/DailyBoxApplication.java
@@ -3,12 +3,14 @@ package com.team1.DailyBox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class DailyBoxApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(DailyBoxApplication.class, args);
 	}
 
